e14d95b14b30d7c162896bff3b824f6d74036f11
EMMANUEL FOTSO KAMGUE <brinkcorp@gmail.com>
2021-05-21 21:16:46 +0000

    readability

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -6,6 +6,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'chat_screen.dart';
 import 'package:modal_progress_hud/modal_progress_hud.dart';
 
+
+//Readability
 class RegistrationScreen extends StatefulWidget {
   static String id = 'registration_screen';
 
